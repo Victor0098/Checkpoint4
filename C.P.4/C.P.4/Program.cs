4fe4fb2a504fb9c38594897a8d8763af8fbd6974
Victor <victor.soderman@outlook.com>
2018-11-02 15:22:55 +0100

    Added projectfiles

---
+++ C.P.4/C.P.4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C.P._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

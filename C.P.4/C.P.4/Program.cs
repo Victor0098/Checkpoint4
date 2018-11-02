8b73e670a77762d0dc9776e79e6a999e0698d4a6
Victor <victor.soderman@outlook.com>
2018-11-02 16:01:30 +0100

    added movienames

--- C.P.4/C.P.4/Program.cs
+++ C.P.4/C.P.4/Program.cs
@@ -9,6 +9,13 @@ namespace C.P._4
             Console.WriteLine("Hello World!");
             Console.WriteLine("Best movies Ever!");
 
+            Console.WriteLine("Copyright Victor Söderman 2018 :)");
+
+            Console.WriteLine("Feature/Movie-1:","Inferno of Punishment");
+            Console.WriteLine("Feature/Movie-2:", "Maximum Assassination");
+            Console.WriteLine("Feature/Movie-3:", "Fist of Domination");
+            Console.WriteLine("Feature/Movie-4:", "Fatal Execution");
+            Console.WriteLine("Feature/Movie-5:", "Extreme Termination");
         }
     }
 }

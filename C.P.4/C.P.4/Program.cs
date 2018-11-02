4bf3593c55b1f2423839bb3e3cad8540559b1acf
Victor <victor.soderman@outlook.com>
2018-11-02 16:20:33 +0100

    changed to 0 movies in develop

--- C.P.4/C.P.4/Program.cs
+++ C.P.4/C.P.4/Program.cs
@@ -11,11 +11,7 @@ namespace C.P._4
 
             Console.WriteLine("Copyright Victor Söderman 2018 :)");
 
-            Console.WriteLine("Feature/Movie-1: Inferno of Punishment");
-            Console.WriteLine("Feature/Movie-2: Maximum Assassination");
-            Console.WriteLine("Feature/Movie-3: Fist of Domination");
-            Console.WriteLine("Feature/Movie-4: Fatal Execution");
-            Console.WriteLine("Feature/Movie-5: Extreme Termination");
+            
         }
     }
 
